d5e0da17dfeb128ec177b7b5ce1f4f950546e4f5
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2021-11-30 12:56:40 -0800

    restructured folders
    - installed dependencies for
    - server

---
+++ myspace/client/src/components/NavigationBar/NavigationBar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+//import styled components
+import { Wrapper, Content , Links, Container} from './NavigationBar.styles';
+//import material components
+import GroupIcon from '../../../client/node_modules/@mui/icons-material/Group';
+//import react router link
+import { Link } from 'react-router-dom';
+const NavigationBar = () => {
+    return (
+        <Wrapper>
+            <Content>
+                <Link to='/'><GroupIcon/></Link>
+
+                <Container>
+                <Link to='/login'>Login</Link>
+                <Link to='/signup'>Sign Up</Link>
+                </Container>
+            </Content>
+        </Wrapper>
+    );
+}
+
+export default NavigationBar;
