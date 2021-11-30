f1f3f41006ae5aae4bd3a9fb33cea8fa2938832d
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2021-11-30 11:28:02 -0800

    created the sign in form

--- myspace/src/App.js
+++ myspace/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 //import my components
 import NavigationBar from './components/NavigationBar/NavigationBar';
+import SignForm from './components/SignForm/SignForm';
 function App() {
   return (
     <div className="App">
       <NavigationBar/>
+      <SignForm/>
     </div>
   );
 }
---
+++ myspace/src/components/SignForm/SignForm.styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+export const Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 400px;
+    width: 300px;
+    padding: 20px;
+`;
+export const ButtonContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+`;
+export const TextSpan = styled.span`
+    color: blue;
+    font-size: 20px;
+    text-align: center;
+`;
